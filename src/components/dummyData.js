f31c2fd8206834c42133a6e7cb3e44a324a09cb6
kmsgm <kmsgm@snu.ac.kr>
2022-10-30 20:36:53 +0900

    modify dummyData

--- src/components/dummyData.js
+++ src/components/dummyData.js
@@ -1,54 +1,54 @@
 export const dummyData = JSON.stringify({
   "startName" : "출발지",
-  "startX" : "127.103259",
-  "startY" : "37.402688",
-  "startTime" : "201708081103",
+  "startX" : "127.103258",
+  "startY" : "37.402687",
+  "startTime" : "201708081102",
   "endName" : "도착지",
-  "endX" : "127.142571",
-  "endY" : "37.414382",
+  "endX" : "127.142570",
+  "endY" : "37.414381",
   "viaPoints" :
     [
       {
         "viaPointId" : "test01",
         "viaPointName" : "name01",
-        "viaX" : "127.103790" ,
-        "viaY" : "37.399569"
+        "viaX" : "127.103789" ,
+        "viaY" : "37.399568"
       },
       {
         "viaPointId" : "test02",
         "viaPointName" : "name02",
-        "viaX" : "127.108913" ,
-        "viaY" : "37.402748"
+        "viaX" : "127.108912" ,
+        "viaY" : "37.402747"
       },
       {
         "viaPointId" : "test03",
         "viaPointName" : "name03",
-        "viaX" : "127.113403" ,
-        "viaY" : "37.397153"
+        "viaX" : "127.113402" ,
+        "viaY" : "37.397152"
       },
       {
         "viaPointId" : "test04",
         "viaPointName" : "name04",
-        "viaX" : "127.121210" ,
-        "viaY" : "37.410135"
+        "viaX" : "127.121209" ,
+        "viaY" : "37.410134"
       },
       {
         "viaPointId" : "test05",
         "viaPointName" : "name05",
-        "viaX" : "127.123296" ,
-        "viaY" : "37.399400"
+        "viaX" : "127.123295" ,
+        "viaY" : "37.399399"
       },
       {
         "viaPointId" : "test06",
         "viaPointName" : "name06",
-        "viaX" : "127.130933" ,
-        "viaY" : "37.406327"
+        "viaX" : "127.130932" ,
+        "viaY" : "37.406326"
       },
       {
         "viaPointId" : "test07",
         "viaPointName" : "name07",
-        "viaX" : "127.127337" ,
-        "viaY" : "37.413227"
+        "viaX" : "127.127336" ,
+        "viaY" : "37.413226"
       }
     ],
   "reqCoordType" : "WGS84GEO",
