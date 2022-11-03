56b675921a2e8eec92191bd119ba7a7e86008ea7
jaejae2374 <jaejae2374@snu.ac.kr>
2022-11-03 17:04:52 +0900

    add Course Post

--- backend/team12/course/serializers.py
+++ backend/team12/course/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from course.models import *
+from team12.exceptions import FieldError
 
 class PointSerializer(serializers.ModelSerializer):
     """
@@ -11,7 +12,7 @@ class PointSerializer(serializers.ModelSerializer):
             'latitude',
             'longitude',
             'idx'
-        )
+        )     
 
 class CourseSerializer(serializers.ModelSerializer):
     """
@@ -19,11 +20,71 @@ class CourseSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Course 
-        fields = '__all__'
+        fields = (
+            'title',
+            'description',
+            'category',
+            'u_counts',
+            'e_time',
+            'distance'
+        )
 
-class CourseRetrieveSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        title = data.get('title')
+        missing_fields = []
+        if title:
+            if not len(title) > 0:
+                raise FieldError("title required.")
+        else:
+            missing_fields.append("title")
+        
+        description = data.get('description')
+        if description:
+            if not len(description) > 10:
+                raise FieldError("description should be more than 10 characters.")
+        else:
+            missing_fields.append("description")
+
+        e_time = data.get("e_time")
+        if not e_time:
+            missing_fields.append("e_time")
+        distance = data.get("distance")
+        if not distance:
+            missing_fields.append("distance")
+
+        points = self.context.get("points", [])
+        if points:
+            if not len(points) > 1:
+                raise FieldError("points should be more than 2.")
+            else:
+                if len(set(map(lambda x: tuple(x.keys()), points))) != 1:
+                    raise FieldError("points keys must be 'longitude', 'latitude'.")
+        else:
+            missing_fields.append("points")
+        
+        if len(missing_fields) > 0:
+            raise FieldError(f"{missing_fields} fields missing.")
+        return data
+
+    def create(self, validated_data):
+        course = Course.objects.create(**validated_data)
+        points = self.context['points']
+        points_list = []
+        for idx, point in enumerate(points):
+            points_list.append(
+                Point(
+                    course=course,
+                    longitude=point['longitude'],
+                    latitude=point['latitude'],
+                    idx=idx
+                )
+            )
+        Point.objects.bulk_create(points_list)
+        return course
+
+class CourseDetailSerializer(serializers.ModelSerializer):
     """
-    Course Model Retrieve Serializer
+    Course Model Detail Serializer
     """
     points = serializers.SerializerMethodField()
     class Meta:
