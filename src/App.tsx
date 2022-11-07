c5b22395abe755e6ce324c0ab4823e16ee0a20e9
kmsgm <kmsgm@snu.ac.kr>
2022-11-07 23:25:48 +0900

    apply eslint to whole code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 import React from 'react';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import CourseDetail from "./contatiners/CourseDetail";
 import SearchPage from './components/SearchPage';
 import TMap from './components/TMap';
+=======
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import CourseDetail from './contatiners/CourseDetail';
+>>>>>>> 577b162 (set eslint.json)
 
 function App() {
   return (
@@ -11,8 +18,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="*" element={<h1>Not Found</h1>} />
+<<<<<<< HEAD
           <Route path="map" element={<TMap />} />
           <Route path="search" element={<SearchPage />} />
+=======
+          <Route path="detail" element={<CourseDetail courseID={1} />} />
+>>>>>>> 577b162 (set eslint.json)
         </Routes>
       </BrowserRouter>
     </div>
