2907d35d2788a965ecff92e2fe6be5ffc004aa35
kmsgm <kmsgm@snu.ac.kr>
2022-11-07 23:32:09 +0900

    resolve conflict

--- src/components/SearchMap.js
+++ src/components/SearchMap.js
@@ -11,10 +11,12 @@ function SearchMap(keyword) {
 
   useEffect(() => {
     if (!map) return
-    const ps = new kakao.maps.services.Places()
+    const ps = new kakao.maps.services.Places();
 
-    if (keyword.keyword !== "") {
-      ps.keywordSearch(keyword.keyword, (data, status, _pagination) => {
+    const { Keyword } = keyword;
+
+    if (Keyword !== "") {
+      ps.keywordSearch(Keyword, (data, status, _pagination) => {
         if (status === kakao.maps.services.Status.OK) {
           // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
           // LatLngBounds 객체에 좌표를 추가합니다
