83ea7b0476084bdb2ed56d8e879f7168d99e491a
syrius527 <djlee1031@naver.com>
2022-11-09 00:27:43 +0900

    Component dir to components dir

--- src/Container/CourseList/CourseList.tsx
+++ src/Container/CourseList/CourseList.tsx
@@ -1,84 +1,96 @@
-import React, { useEffect} from "react";
-import CourseListElement from "../../Component/CourseListElement.tsx/CourseListElement";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router";
-import { selectCourse, fetchCoursesParams, fetchCourses, fetchCourse } from "../../store/slices/course";
-import SearchBox from "../../Component/SearchBox/SearchBox";
-import Header from "../../Component/Header/Header";
-import { AppDispatch } from "../../store";
-import ListFilter from "../../Component/ListFilter/ListFilter";
+import React, { useEffect } from 'react';
+import CourseListElement from '../../components/CourseListElement.tsx/CourseListElement';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import {
+  selectCourse,
+  fetchCoursesParams,
+  fetchCourses,
+  fetchCourse,
+} from '../../store/slices/course';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import Header from '../../components/Header/Header';
+import { AppDispatch } from '../../store';
+import ListFilter from '../../components/ListFilter/ListFilter';
 
 const CourseList = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch<AppDispatch>();
-    const courseState = useSelector(selectCourse);
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const courseState = useSelector(selectCourse);
 
-    type CourseElemType = {
-        id: number;
-        title: string;
-        description: string;
-        category: string;
-        grade: number;
-        f_counts: number;
-    }
+  type CourseElemType = {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    grade: number;
+    f_counts: number;
+  };
 
-    const init = () => {
-        const params: fetchCoursesParams = {
-            page: 1,
-            category: localStorage.getItem("CATEGORY_KEY") ?? "drive",
-            search_keyword: localStorage.getItem("SEARCH_KEY") ?? null,
-            filter: localStorage.getItem("FILTER") ?? null,
-        }
-        dispatch(fetchCourses(params));
+  const init = () => {
+    const params: fetchCoursesParams = {
+      page: 1,
+      category: localStorage.getItem('CATEGORY_KEY') ?? 'drive',
+      search_keyword: localStorage.getItem('SEARCH_KEY') ?? null,
+      filter: localStorage.getItem('FILTER') ?? null,
     };
+    dispatch(fetchCourses(params));
+  };
 
-    useEffect(() => {
-        init();
-    }, []);
+  useEffect(() => {
+    init();
+  }, []);
 
-    const korCategory = (ctgry: string) => {
-        if (ctgry === "drive") return "드라이브";
-        else if (ctgry === "bike") return "바이크 라이드";
-        else if (ctgry === "cycle") return "자전거 라이드";
-        else if (ctgry === "run") return "런닝/산책";
-        else if (ctgry === "") return "";
-        else return "";
-    }
+  const korCategory = (ctgry: string) => {
+    if (ctgry === 'drive') return '드라이브';
+    else if (ctgry === 'bike') return '바이크 라이드';
+    else if (ctgry === 'cycle') return '자전거 라이드';
+    else if (ctgry === 'run') return '런닝/산책';
+    else if (ctgry === '') return '';
+    else return '';
+  };
 
-    return (
-        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-            <h2>Courses List</h2>
-            <Header />
-            <h3>{korCategory(localStorage.getItem("CATEGORY_KEY") ?? "drive")}</h3>
-            <div className="course-list">
-                <div style={{display: "flex", justifyContent: "center"}}>
-                    <SearchBox searchKey={localStorage.getItem("SEARCH_KEY")}/>
-                    <ListFilter />
-                </div>
-                {courseState.courses.map((course) => {
-                    const { id, title, description, u_counts, e_time } = course;
-
-                    const clickTitle = async (id: CourseElemType["id"]) => {
-                        await dispatch(fetchCourse(id));
-                        navigate(`/course/${id}`);
-                    };
-
-                    return (
-                        <CourseListElement
-                            key={`course${id}`}
-                            id={id}
-                            title={title}
-                            description={description}
-                            grade={4.5}
-                            u_counts={u_counts}
-                            e_time={e_time}
-                            showDetail={() => clickTitle(id)}
-                        />
-                    )
-                })}
-            </div>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h2>Courses List</h2>
+      <Header />
+      <h3>{korCategory(localStorage.getItem('CATEGORY_KEY') ?? 'drive')}</h3>
+      <div className="course-list">
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <SearchBox searchKey={localStorage.getItem('SEARCH_KEY')} />
+          <ListFilter />
         </div>
-    )
+        {courseState.courses.map((course) => {
+          const { id, title, description, u_counts, e_time } = course;
+
+          const clickTitle = async (id: CourseElemType['id']) => {
+            await dispatch(fetchCourse(id));
+            navigate(`/course/${id}`);
+          };
+
+          return (
+            <CourseListElement
+              key={`course${id}`}
+              id={id}
+              title={title}
+              description={description}
+              grade={4.5}
+              u_counts={u_counts}
+              e_time={e_time}
+              showDetail={() => clickTitle(id)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CourseList;
--- src/Container/MainPage/MainPage.tsx
+++ src/Container/MainPage/MainPage.tsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
-import Header from "../../Component/Header/Header";
-import SearchBox from "../../Component/SearchBox/SearchBox";
+import React, { useEffect } from 'react';
+import Header from '../../components/Header/Header';
+import SearchBox from '../../components/SearchBox/SearchBox';
 
 const MainPage = () => {
-    useEffect(() => {
-        localStorage.removeItem("CATEGORY_KEY");
-        localStorage.removeItem("SEARCH_KEY");
-        localStorage.removeItem("FILTER");
-    }, []);
+  useEffect(() => {
+    localStorage.removeItem('CATEGORY_KEY');
+    localStorage.removeItem('SEARCH_KEY');
+    localStorage.removeItem('FILTER');
+  }, []);
 
-    return (
-        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-            <h2>Main Page</h2>
-            <Header />
-            <SearchBox searchKey={localStorage.getItem("SEARCH_KEY") ?? ""} />
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h2>Main Page</h2>
+      <Header />
+      <SearchBox searchKey={localStorage.getItem('SEARCH_KEY') ?? ''} />
+    </div>
+  );
 };
-// 
+//
 export default MainPage;
