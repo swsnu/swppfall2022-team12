1c0bef0b1dc98d9097c54a012f4fe939509ce7ec
syrius527 <djlee1031@naver.com>
2022-11-09 17:26:29 +0900

    add create new course button in main page

--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,14 +1,22 @@
+import { Button } from '@mui/material';
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
 import Header from '../../components/Header/Header';
 import SearchBox from '../../components/SearchBox/SearchBox';
 
 function MainPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     localStorage.removeItem('CATEGORY_KEY');
     localStorage.removeItem('SEARCH_KEY');
     localStorage.removeItem('FILTER');
   }, []);
+
+  const onClickCreateCourse = () => {
+    navigate('search');
+  };
 
   return (
     <div
@@ -22,6 +30,9 @@ function MainPage() {
       <h2>Main Page</h2>
       <Header />
       <SearchBox searchKey={localStorage.getItem('SEARCH_KEY') ?? ''} />
+      <Button variant="outlined" onClick={onClickCreateCourse}>
+        Create New Course
+      </Button>
     </div>
   );
 }
