40d4fca26ff857bdae6503632fa066d278ddcbcf
syrius527 <djlee1031@naver.com>
2022-11-21 15:34:39 +0900

    login-feature backend

--- backend/team12/user/models.py
+++ backend/team12/user/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
+from tag.models import Tag
 
 # TODO: customize User. Add fields (Favor, History, Tag)
 class User(AbstractUser):
-
-     class Meta:
-         db_table = 'user'
+  tags = models.models.ManyToManyField(Tag, verbose_name=_(""))
+  class Meta:
+    db_table = 'user'
--- backend/team12/user/views.py
+++ backend/team12/user/views.py
@@ -1,3 +1,44 @@
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from user.models import User
+import json
 
 # Create your views here.
+def signup(request):
+  if request.method == 'POST':
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    new_user = User.objects.create_user(username, password)
+    new_user.save()
+
+    return HttpResponse(json.dumps({ 'id': new_user.id, 'username': new_user.username }), status=201)
+  else:
+    return HttpResponseNotAllowed(['POST'])
+
+def signin(request):
+  if request.method == 'POST':
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username, password)
+
+    if user is not None:
+      login(request, user)
+      return HttpResponse(json.dumps({ 'id': user.id, 'username': user.username }), status=204)
+    else:
+        return HttpResponse(status=401)
+
+  else:
+      return HttpResponseNotAllowed(['POST'])
+
+def logout(request):
+  if request.method == 'GET':
+    if request.user.is_authenticated:
+      logout(request)
+      return HttpResponse(status=204)
+    else:
+      return HttpResponse(status=401)
+
+  else:
+    return HttpResponseNotAllowed(['GET'])
