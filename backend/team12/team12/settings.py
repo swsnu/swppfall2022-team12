a9e31da140bc7fe669d55ebef559bc21a1d6a00f
jaejae2374 <jaejae2374@snu.ac.kr>
2022-12-06 18:17:22 +0900

    deploy

--- backend/team12/team12/settings.py
+++ backend/team12/team12/settings.py
@@ -108,10 +108,22 @@ SIMPLE_JWT = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'team12',
+        'USER': 'team12',
+        'PASSWORD': 'team12',
+        'HOST': 'swpp-team12.cq3boo3w9eua.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
