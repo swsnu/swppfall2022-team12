7840be87d9be6d3915641103f58753cd8246ad38
kmsgm <minseo.kim@friendli.ai>
2022-12-09 16:01:13 +0900

    fix button position

--- frontend/src/containers/CourseCreate/PostCourse.tsx
+++ frontend/src/containers/CourseCreate/PostCourse.tsx
@@ -107,7 +107,6 @@ export default function PostCourse() {
       >
         <Button
           style={{
-            marginRight: 15,
             height: 50,
             boxShadow:
               'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
