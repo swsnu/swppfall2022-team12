7f2cb13ed1c0f336411e227ad1878e7e3042b7e1
kmsgm <kmsgm@snu.ac.kr>
2022-12-10 22:16:15 +0900

    split design

--- frontend/src/containers/CourseDetail/DetailPage.tsx
+++ frontend/src/containers/CourseDetail/DetailPage.tsx
@@ -191,6 +191,8 @@ export default function CourseDetail() {
           zIndex: 1,
           backgroundColor: 'white',
           textAlign: 'center',
+          boxShadow:
+            'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
         }}
       >
         <div style={{ margin: '30px 30px 0 30px' }}>
