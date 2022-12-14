05786db5d2dc2b08774ff5ffb282fbfcfb1c7b87
jaejae2374 <jaejae2374@snu.ac.kr>
2022-12-14 17:05:42 +0900

    final

--- backend/team12/course/views.py
+++ backend/team12/course/views.py
@@ -55,9 +55,7 @@ class CourseViewSet(
         data["author"] = user_id
         serializer = self.get_serializer(data=data, context=context)
         serializer.is_valid(raise_exception=True)
-        print("done.")
         course = serializer.save()
-        print(course)
         return Response(CourseDetailSerializer(course).data, status=status.HTTP_200_OK)
 
     # DELETE /course/:courseId
--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git pull origin main
 cd backend/team12
 pip3 install -r requirements.txt
 sudo systemctl restart nginx
-gunicorn team12.wsgi --bind 0.0.0.0:8000
+gunicorn team12.wsgi --bind 0.0.0.0:8000 --daemon
