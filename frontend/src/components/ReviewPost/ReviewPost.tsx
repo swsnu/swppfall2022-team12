945bf064fc6567e61f95818d896817e7bee8dc45
Alexjonghyun <whywhy093@snu.ac.kr>
2022-12-14 13:38:32 +0900

    lint and toast error

--- frontend/src/components/ReviewPost/ReviewPost.tsx
+++ frontend/src/components/ReviewPost/ReviewPost.tsx
@@ -2,6 +2,8 @@ import { Input, Button } from 'antd';
 import axios from 'axios';
 import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
 
 const { TextArea } = Input;
 
@@ -18,8 +20,8 @@ export default function ReviewPost(prop: ReviewPostProp) {
   const [courseId, setCourseId] = useState<number>(1);
 
   const post = () => {
-    if(prop.courseAuthor === sessionStorage.getItem("username")){
-      alert("자신이 작성한 코스에 리뷰를 남길 수 없습니다.");
+    if (prop.courseAuthor === sessionStorage.getItem('username')) {
+      toast.error('자신이 작성한 코스에 리뷰를 남길 수 없습니다.');
       return;
     }
     if (content.length === 0) {
