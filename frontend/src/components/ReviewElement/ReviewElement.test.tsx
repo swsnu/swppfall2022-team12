2a96f9fb61cfc19b6f1678d6719b2dffe912dd45
Alexjonghyun <88361017+Alexjonghyun@users.noreply.github.com>
2022-12-18 00:02:31 +0700

    Update ReviewElement.test.tsx

--- frontend/src/components/ReviewElement/ReviewElement.test.tsx
+++ frontend/src/components/ReviewElement/ReviewElement.test.tsx
@@ -1,5 +1,6 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import axios from 'axios';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
 
 import { renderWithProviders } from '../../test-utils/mocks';
 import ReviewElement from './ReviewElement';
@@ -8,6 +9,10 @@ describe('<ReviewElement />', () => {
   jest.spyOn(axios, 'put').mockImplementation(() => {
     return Promise.resolve({});
   });
+  jest.spyOn(axios, 'delete').mockImplementation(() => {
+    return Promise.resolve({});
+  });
+  window.alert = jest.fn();
 
   it('should render without errors', () => {
     renderWithProviders(
@@ -22,19 +27,118 @@ describe('<ReviewElement />', () => {
         setChange={jest.fn()}
       />,
     );
-    const likeButton = screen.getByText('like');
-    fireEvent.click(likeButton!);
-    const editButton = screen.getByText('edit');
-    fireEvent.click(editButton!);
-    const star = screen.getAllByTestId('star');
-    fireEvent.click(star[0]!);
+  });
 
+  it('should not like if not logged in', () => {
+    renderWithProviders(
+      <ReviewElement
+        id={3}
+        content="test"
+        likes={0}
+        author="test author"
+        rate={5}
+        created_at="2022-11-23T02:37:12.007952Z"
+        change={0}
+        setChange={jest.fn()}
+      />,
+    );
+    const likeButton = screen.getByTestId('like');
+    fireEvent.click(likeButton!);
+  });
+
+  it('should not like if he is an author', () => {
+    window.sessionStorage.setItem('username', 'test author');
+    renderWithProviders(
+      <ReviewElement
+        id={3}
+        content="test"
+        likes={0}
+        author="test author"
+        rate={5}
+        created_at="2022-11-23T02:37:12.007952Z"
+        change={0}
+        setChange={jest.fn()}
+      />,
+    );
+    const likeButton = screen.getByTestId('like');
+    fireEvent.click(likeButton!);
+  });
+
+  it('should not like if he is an author', () => {
+    window.sessionStorage.setItem('username', 'test author 2');
+    renderWithProviders(
+      <ReviewElement
+        id={3}
+        content="test"
+        likes={0}
+        author="test author"
+        rate={5}
+        created_at="2022-11-23T02:37:12.007952Z"
+        change={0}
+        setChange={jest.fn()}
+      />,
+    );
+    const likeButton = screen.getByTestId('like');
+    fireEvent.click(likeButton!);
+  });
+
+  it('should not edit if he is not an author', () => {
+    window.sessionStorage.setItem('username', 'test author 2');
+    renderWithProviders(
+      <ReviewElement
+        id={3}
+        content="test"
+        likes={0}
+        author="test author"
+        rate={5}
+        created_at="2022-11-23T02:37:12.007952Z"
+        change={0}
+        setChange={jest.fn()}
+      />,
+    );
+    const editButton = screen.getByTestId('edit');
+    fireEvent.click(editButton!);
+  });
+
+  it('should edit if he is an author', () => {
+    window.sessionStorage.setItem('username', 'test author');
+    renderWithProviders(
+      <ReviewElement
+        id={3}
+        content="test"
+        likes={0}
+        author="test author"
+        rate={5}
+        created_at="2022-11-23T02:37:12.007952Z"
+        change={0}
+        setChange={jest.fn()}
+      />,
+    );
+    const editButton = screen.getByTestId('edit');
+    fireEvent.click(editButton!);
+    const starButtons = screen.getAllByTestId('star');
+    fireEvent.click(starButtons[2]);
     const editting = screen.getByTestId('editting');
     fireEvent.change(editting, { target: { value: 'TEST' } });
-    const editButton2 = screen.getAllByText('edit');
-    fireEvent.click(editButton2[1]!);
+    const confirmButton = screen.getByTestId('confirm');
+    fireEvent.click(confirmButton!);
+  });
 
-    const deleteButton = screen.getByText('delete');
+  it('should delete if he is an author', () => {
+    window.sessionStorage.setItem('username', 'test author');
+    renderWithProviders(
+      <ReviewElement
+        id={3}
+        content="test"
+        likes={0}
+        author="test author"
+        rate={5}
+        created_at="2022-11-23T02:37:12.007952Z"
+        change={0}
+        setChange={jest.fn()}
+      />,
+    );
+    const deleteButton = screen.getByTestId('delete');
     fireEvent.click(deleteButton!);
   });
 });
